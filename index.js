99550f7ca8a98a2f280d5c1f327ad3fdb666cb64
yukako <114702943+y-tom@users.noreply.github.com>
2025-08-11 17:39:08 +0900

    投稿一覧表示機能追加、DBへの保存機能実装、削除機能実装、管理者機能実装

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict';
 const http = require('node:http');
+const auth = require('http-auth')
 const router = require('./lib/router');
 
-const server = http.createServer((req, res) => {
+const basic = auth.basic({
+    realm:'Enter username and password.',
+    file:'./users.htpasswd'
+})
+  const server = http.createServer(basic.check((req,res) => {
     router.route(req,res);
-  })
+  }))
   .on('error', e => {
     console.error('Server Error', e);
   })
